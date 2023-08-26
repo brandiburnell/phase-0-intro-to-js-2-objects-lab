52af9d133e7d515bd62a1831504e454f464df6be
Brandi Burnell <brandiburnell6@gmail.com>
2023-08-26 17:23:01 -0600

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Brandi",
+    streetAddress: "10830 W Evans Ave"
+};
+
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+   const smallerObj = {...employee};
+   delete smallerObj[key];
+
+   return smallerObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
